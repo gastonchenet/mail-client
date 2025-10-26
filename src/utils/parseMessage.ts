488db9d319e96aff8a122e316844528a0e472adf
Gaston Chenet <contact@gastonchenet.com>
2025-10-26 23:05:14 +0100

    Fox

--- src/utils/parseMessage.ts
+++ src/utils/parseMessage.ts
@@ -9,7 +9,9 @@ export default function parseMessage(message: RawMessage): Message {
     subject: message.subject,
     preview: message.preview,
     starred: message.starred,
+    seen: message.seen,
     date: moment(message.date),
     created_at: moment(message.created_at),
+    deleted_at: moment(message.deleted_at),
   };
 }
